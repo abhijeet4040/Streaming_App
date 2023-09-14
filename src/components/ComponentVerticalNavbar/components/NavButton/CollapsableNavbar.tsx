62dde11bc85ec99ba3cd5790d8ed7760e8f15597
abhijeet <abhijeet.k@binmile.com>
2023-09-14 19:32:55 +0530

    made responsive

--- src/components/ComponentVerticalNavbar/components/NavButton/CollapsableNavbar.tsx
+++ src/components/ComponentVerticalNavbar/components/NavButton/CollapsableNavbar.tsx
@@ -3,7 +3,7 @@ import { CollapsibleIcon } from '../../../../assets/Images'
 
 export const CollapsibleNavbar = () => {
   return (
-    <div className='max-w-[20px] self-end absolute sm:hidden'>
+    <div className='max-w-[20px] self-end fixed sm:hidden right-[50px] top-[50px]'>
         <img src={CollapsibleIcon} alt="#" />
     </div>
   )
